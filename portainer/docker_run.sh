2b2d4b942e188d1263837240ffcacd965aa47de4
tlw_ray <tlw_ray@163.com>
2020-03-20 22:11:21 +0800

    clean portainer and draw

--- portainer/docker_run.sh
+++ portainer/docker_run.sh
@@ -2,7 +2,7 @@ docker run \
 -d \
 -p 50380:9000 \
 -v "/var/run/docker.sock:/var/run/docker.sock" \
--v $PWD/data:/data \
+-v ~/portainer/data:/data \
 --name AP_Portainer \
 --restart always \
 portainer/portainer
